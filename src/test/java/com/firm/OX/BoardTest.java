576e86fc1375c4af03aac1411708e2d4fabe7d59
sehta7@gmail.com <sehta7@gmail.com>
2019-03-17 19:02:50 +0100

    test to check if board doesn't have size equal zero

--- src/test/java/com/firm/OX/BoardTest.java
+++ src/test/java/com/firm/OX/BoardTest.java
@@ -13,12 +13,17 @@ public class BoardTest {
 
     @Test
     public void testIfBoardCanBeCreated() {
-        Board board = new Board(new Size(0,0));
+        Board board = new Board(new Size(1,1));
     }
 
     @Test(dataProviderClass = BoardDP.class, dataProvider = "sizes")
     public void testIfBoardHasSizeGivenByUser(Size size) {
         Board board = new Board(size);
         assert (board.getSize().equals(size)) : "Size of board aren't set properly";
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testIfBoardSizeCanNotBeZero() {
+        Board board = new Board(new Size(0, 0));
     }
 }
