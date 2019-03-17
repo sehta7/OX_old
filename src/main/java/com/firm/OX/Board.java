07c4c6f490c308e1aa3ce7da7196ce4670b11962
sehta7@gmail.com <sehta7@gmail.com>
2019-03-17 19:03:29 +0100

    add IllegalArgumentException in Board class

--- src/main/java/com/firm/OX/Board.java
+++ src/main/java/com/firm/OX/Board.java
@@ -5,6 +5,9 @@ public class Board {
     private Size size;
 
     public Board(Size size) {
+        if (size.equals(new Size(0, 0))){
+            throw new IllegalArgumentException();
+        }
         this.size = size;
     }
 
