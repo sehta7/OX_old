add4a123964d916b5d8efebb370d362b68f89f5e
sehta7@gmail.com <sehta7@gmail.com>
2019-03-17 17:42:19 +0100

    testing Board class

---
+++ src/test/java/com/firm/OX/BoardTest.java
@@ -0,0 +1,18 @@
+package com.firm.OX;
+
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author Ola Podorska
+ *
+ */
+
+@Test
+public class BoardTest {
+
+    @Test
+    public void testIfBoardCanBeCreated() {
+        Board board = new Board();
+    }
+}
