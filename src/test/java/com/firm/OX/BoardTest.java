0ae08a9b90cf04d319f950fbe71b43e4d3194644
sehta7@gmail.com <sehta7@gmail.com>
2019-03-17 17:53:00 +0100

    test to check size of board

--- src/test/java/com/firm/OX/BoardTest.java
+++ src/test/java/com/firm/OX/BoardTest.java
@@ -15,4 +15,10 @@ public class BoardTest {
     public void testIfBoardCanBeCreated() {
         Board board = new Board();
     }
+
+    @Test(dataProviderClass = BoardDP.class, dataProvider = "sizes")
+    public void testIfBoardHasSizeGivenByUser(Size size) {
+        Board board = new Board(size);
+        assert (board.getSize().equals(size)) : "Size of board aren't set properly";
+    }
 }
