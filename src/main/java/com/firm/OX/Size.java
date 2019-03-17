338e3a31f98e0a657608389dacb7192c29e64a75
sehta7@gmail.com <sehta7@gmail.com>
2019-03-17 19:04:07 +0100

    generate equals() and hashCode() methods in Size class

--- src/main/java/com/firm/OX/Size.java
+++ src/main/java/com/firm/OX/Size.java
@@ -1,5 +1,7 @@
 package com.firm.OX;
 
+import java.util.Objects;
+
 public class Size {
 
     private int length;
@@ -8,5 +10,19 @@ public class Size {
     public Size(int length, int height) {
         this.length = length;
         this.height = height;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Size size = (Size) o;
+        return length == size.length &&
+                height == size.height;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(length, height);
     }
 }
