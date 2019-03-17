7e0e7ac279a5bdac2185eeb51f88e7199dec7b15
sehta7@gmail.com <sehta7@gmail.com>
2019-03-17 11:45:08 +0100

    test creation of empty field

---
+++ src/test/java/com/firm/OX/FieldTest.java
@@ -0,0 +1,18 @@
+package com.firm.OX;
+
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author Ola Podorska
+ *
+ */
+
+@Test
+public class FieldTest {
+
+    @Test
+    public void testIfEmptyFieldCanBeCreated() {
+        Field field = new EmptyField();
+    }
+}
