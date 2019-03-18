6a7faf7bd5a0dd46b4587883231b388c0d35b3f0
sehta7@gmail.com <sehta7@gmail.com>
2019-03-18 22:34:00 +0100

    not perfect method of drawing grid

--- src/main/java/com/firm/OX/BoardCreator.java
+++ src/main/java/com/firm/OX/BoardCreator.java
@@ -5,18 +5,37 @@ import java.util.Collections;
 public class BoardCreator {
 
     public boolean drawEmptyGrid() {
-        for (int y = 0; y < 3; y++){
-            for (int i = 0; i < 2; i++){
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 2; j++){
                 String repeated = String.join("", Collections.nCopies(5, " "));
                 System.out.print(repeated);
                 System.out.print("|");
                 System.out.print(repeated);
                 System.out.println("|");
             }
-            if (y == 2){
+            if (i == 2){
                 break;
             }
             System.out.println(String.join("", Collections.nCopies(17, "-")));
+        }
+        return true;
+    }
+
+    public boolean drawGridWithCoordinates() {
+
+        int n = 3;
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 3; j++){
+                String pause = "------";
+                String place = "|     ";
+                System.out.println(String.join("", Collections.nCopies(n, pause)));
+                for (int x = 0; x < n; x++){
+                    String coordinates = "|" + (i + x) + "," + (j + x) + "  ";
+                    System.out.print(coordinates);
+                }
+                System.out.println();
+                System.out.println(String.join("", Collections.nCopies(n, place)));
+            }
         }
         return true;
     }
