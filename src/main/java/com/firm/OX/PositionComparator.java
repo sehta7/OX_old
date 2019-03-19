7213d7b8c7436a3877a7e4245003920ce1739af9
Ola <aleksandra.podorska@gmail.com>
2019-03-19 12:42:04 +0100

    comparator to sort positions in queue

---
+++ src/main/java/com/firm/OX/PositionComparator.java
@@ -0,0 +1,25 @@
+package com.firm.OX;
+
+import java.util.Comparator;
+
+/**
+ * @author Ola Podorska
+ */
+public class PositionComparator implements Comparator<Position> {
+    @Override
+    public int compare(Position p1, Position p2) {
+        if (p1.getRow() < p2.getRow()){
+            return -1;
+        } else if(p1.getRow() > p2.getRow()){
+            return 1;
+        } else {
+            if (p1.getColumn() < p2.getColumn()){
+                return -1;
+            } else if(p1.getColumn() > p2.getColumn()){
+                return 1;
+            } else{
+                return 0;
+            }
+        }
+    }
+}
