01e1c4a510813e006e736d8dfc0bbb1cca96ad25
sehta7@gmail.com <sehta7@gmail.com>
2019-03-19 19:39:58 +0100

    add needed fields to Player class and equals(), hashCode() methods

--- src/main/java/com/firm/OX/Player.java
+++ src/main/java/com/firm/OX/Player.java
@@ -1,10 +1,28 @@
 package com.firm.OX;
 
+import java.util.Objects;
+
 /**
  * @author Ola Podorska
  */
 public class Player {
-    public String getCharacter() {
-        return "";
+
+    private String name;
+    private Characters character;
+    private int points;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Player player = (Player) o;
+        return points == player.points &&
+                Objects.equals(name, player.name) &&
+                character == player.character;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, character, points);
     }
 }
