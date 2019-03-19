c27b23f94a0ecd3ce583471f260ebb7269bfb60d
sehta7@gmail.com <sehta7@gmail.com>
2019-03-19 19:40:49 +0100

    prepare GameOptions to remember players

--- src/main/java/com/firm/OX/GameOptions.java
+++ src/main/java/com/firm/OX/GameOptions.java
@@ -1,10 +1,34 @@
 package com.firm.OX;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author Ola Podorska
  */
 public class GameOptions {
-    public void assignPlayers(Player playerOne, String character) {
 
+    private Map<String, Player> players;
+
+    public GameOptions(){
+        players = new HashMap<>();
+    }
+
+    public void assignPlayers(Player playerOne, String character) {
+        players.put(character, playerOne);
+    }
+
+    public boolean isPlayerO(Player player) {
+        if (player.equals(players.get("O"))){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isPlayerX(Player player) {
+        if (player.equals(players.get("X"))){
+            return true;
+        }
+        return false;
     }
 }
