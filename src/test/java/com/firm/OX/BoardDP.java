b073db4ff8e0b4a773b2f9893c993f348f02b801
Ola <aleksandra.podorska@gmail.com>
2019-03-19 11:33:12 +0100

    test sorting positions in queue to draw characters in right order

--- src/test/java/com/firm/OX/BoardDP.java
+++ src/test/java/com/firm/OX/BoardDP.java
@@ -39,4 +39,16 @@ public class BoardDP {
                 {new Size(2, 1)},
         };
     }
+
+    @DataProvider
+    public static Object[][] sort(){
+        return new Object[][]{
+                {new Position(1,2), new Position(3,5), 1},
+                {new Position(2,2), new Position(4,8), 1},
+                {new Position(4,7), new Position(4,7), 0},
+                {new Position(1,6), new Position(1,3), -1},
+                {new Position(3,2), new Position(2,5), -1}
+        };
+    }
+
 }
