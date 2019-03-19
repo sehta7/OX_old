a0879542f18dd63832156a1b77885e8d1d86347e
Ola <aleksandra.podorska@gmail.com>
2019-03-19 10:39:49 +0100

    drawing board with size and given position

--- src/main/java/com/firm/OX/BoardCreator.java
+++ src/main/java/com/firm/OX/BoardCreator.java
@@ -57,8 +57,8 @@ public class BoardCreator {
     }
 
     public boolean drawGridWithGivenPosition(Position position) {
-        drawHeader();
-        int n = position.getRow(), m = position.getColumn(), x = 10;
+        int n = position.getRow(), m = position.getColumn(), x = 60;
+        drawHeader(m);
         for (int i = 0; i < x; i ++){
             for (int j = 0; j < x; j++){
 
@@ -79,13 +79,39 @@ public class BoardCreator {
         return true;
     }
 
-    public void drawHeader(){
-        int n = 10;
+    public void drawHeader(int n){
         System.out.print(" ");
         for (int i = 0; i < n; i++){
-            System.out.print("    " + i + " ");
+            if (i < 10){
+                System.out.print("    " + i + " ");
+            } else{
+                System.out.print("   " + i  + " ");
+            }
+
         }
         System.out.println();
     }
 
+    public boolean drawGridWithGivenPositionAnSize(Position position, Size size) {
+        int n = position.getRow(), m = position.getColumn(), x = size.getHeight(), y = size.getLength();
+        drawHeader(y);
+        for (int i = 0; i < x; i ++){
+            for (int j = 0; j < y; j++){
+
+                if (j == 0){
+                    System.out.print(i + " ");
+                }
+                if (i == n && j == m){
+                    System.out.print("|  O  ");
+                }else{
+                    System.out.print("|     ");
+                }
+            }
+            System.out.println("|");
+            System.out.print("  ");
+            String pause = " _____";
+            System.out.println(String.join("", Collections.nCopies(y, pause)));
+        }
+        return true;
+    }
 }
