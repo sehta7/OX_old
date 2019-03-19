c27f9ba03c1ea4e8f920049fc0e4a99d3834da8f
sehta7@gmail.com <sehta7@gmail.com>
2019-03-19 19:40:20 +0100

    create enum to store characters

---
+++ src/main/java/com/firm/OX/Characters.java
@@ -0,0 +1,16 @@
+package com.firm.OX;
+
+public enum Characters {
+
+    NAUGHT ("O"), CROSS("X");
+
+    private String character;
+
+    Characters(String character) {
+        this.character = character;
+    }
+
+    public String getCharacter(){
+        return character;
+    }
+}
