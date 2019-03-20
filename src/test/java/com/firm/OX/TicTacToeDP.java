b9b0aff765b3d50ebbd8487ab807012c5260dded
Ola <aleksandra.podorska@gmail.com>
2019-03-20 09:55:18 +0100

    test API method which saves all options

---
+++ src/test/java/com/firm/OX/TicTacToeDP.java
@@ -0,0 +1,39 @@
+package com.firm.OX;
+
+import org.testng.annotations.DataProvider;
+
+import java.util.Random;
+
+/**
+ *
+ * @author Ola Podorska
+ *
+ */
+
+public class TicTacToeDP {
+
+    private final static int MAX_ARRAY_SIZE = 4850;
+
+    @DataProvider
+    public static Object[][] options(){
+        Random random = new Random();
+        return new Object[][]{
+                {new Player(randomName()), new Player(randomName()), new Size(random.nextInt(MAX_ARRAY_SIZE), random.nextInt(MAX_ARRAY_SIZE)), random.nextInt(98) + 3},
+                {new Player(randomName()), new Player(randomName()), new Size(random.nextInt(MAX_ARRAY_SIZE), random.nextInt(MAX_ARRAY_SIZE)), random.nextInt(98) + 3},
+                {new Player(randomName()), new Player(randomName()), new Size(random.nextInt(MAX_ARRAY_SIZE), random.nextInt(MAX_ARRAY_SIZE)), random.nextInt(98) + 3},
+                {new Player(randomName()), new Player(randomName()), new Size(random.nextInt(MAX_ARRAY_SIZE), random.nextInt(MAX_ARRAY_SIZE)), random.nextInt(98) + 3},
+                {new Player(randomName()), new Player(randomName()), new Size(random.nextInt(MAX_ARRAY_SIZE), random.nextInt(MAX_ARRAY_SIZE)), random.nextInt(98) + 3}
+        };
+    }
+
+    private static String randomName(){
+        Random random = new Random();
+        int firstASCIIChar = 32;
+        int lastASCIIChar = 126;
+        StringBuilder name = new StringBuilder();
+        for (int i = 0; i < random.nextInt(15); i++){
+            name.append(random.nextInt((lastASCIIChar - firstASCIIChar) + 1) + firstASCIIChar);
+        }
+        return name.toString();
+    }
+}
--- src/test/java/com/firm/OX/TicTacToeTest.java
+++ src/test/java/com/firm/OX/TicTacToeTest.java
@@ -9,4 +9,14 @@ import org.testng.annotations.Test;
 @Test
 public class TicTacToeTest {
 
+    @Test(dataProviderClass = TicTacToeDP.class, dataProvider = "options")
+    public void testIfChosenOptionsAreSaved(Player p1, Player p2, Size size, int numberOfCharacters) {
+        TicTacToe ticTacToe = new TicTacToe();
+        GameOptions gameOptions = new GameOptions();
+        ticTacToe.setGameOpotions(gameOptions);
+        assert (gameOptions.sizeOfBoard().equals(size)) : "Size of board wasn't save in game options";
+        assert (gameOptions.numberOfCharacters() == numberOfCharacters) : "Number of winning characters wasn't save in game options";
+        assert (gameOptions.players().containsValue(p1) && gameOptions.players().containsValue(p2)) : "Players wasn't save in game options";
+        assert (gameOptions.whoStarts().equals(p1)) : "Starting player wasn't save in game options";
+    }
 }
