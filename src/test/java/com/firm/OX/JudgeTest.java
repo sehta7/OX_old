902a15c751bd18ca87b9aeb7f39ef87d1fd07817
Ola <aleksandra.podorska@gmail.com>
2019-03-21 09:28:57 +0100

    test both options of checking neighbours - field has neighbour and doesn't have

--- src/test/java/com/firm/OX/JudgeTest.java
+++ src/test/java/com/firm/OX/JudgeTest.java
@@ -7,11 +7,30 @@ import org.testng.annotations.Test;
  */
 
 @Test
-class JudgeTest {
+public class JudgeTest {
 
     @Test
-    public void testIfJudgeSeekNeighbours() {
-        Judge judge = new Judge();
-        assert (judge.checkNeighbours(field, fields)) : "Judge doesn't see neighbours";
+    public void testIfJudgeDoesNotSeekNeighbours() {
+        Judge judge = new Judge(new Size(15, 15));
+        Field field = new NotEmptyField(new Position(1,1));
+        Positions positions = new Positions(10, new PositionComparator());
+        positions.add(field.getPosition());
+        Player player = new Player();
+        positions.linkPlayerWithPositions(player, field.getPosition());
+        assert (!judge.checkNeighbours(field, positions)) : "Judge saw neighbours";
+    }
+
+    @Test
+    public void testIfJudgeSeeNeighbours() {
+        Judge judge = new Judge(new Size(15, 15));
+        Field field1 = new NotEmptyField(new Position(1, 1));
+        Field field2 = new NotEmptyField(new Position(0 ,0));
+        Positions positions = new Positions(10, new PositionComparator());
+        positions.add(field1.getPosition());
+        positions.add(field2.getPosition());
+        Player player = new Player();
+        positions.linkPlayerWithPositions(player, field1.getPosition());
+        positions.linkPlayerWithPositions(player, field2.getPosition());
+        assert (judge.checkNeighbours(field1, positions)) : "Judge doesn't see neighbours";
     }
 }
