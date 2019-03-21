7efeb13f9af521e30bf3bd43a0369e2641528ff2
Ola <aleksandra.podorska@gmail.com>
2019-03-21 10:11:27 +0100

    testing if judge checks only the same characters

--- src/test/java/com/firm/OX/JudgeTest.java
+++ src/test/java/com/firm/OX/JudgeTest.java
@@ -48,4 +48,23 @@ public class JudgeTest {
         positions.linkPlayerWithPositions(playerX, field2.getPosition());
         assert (judge.isOtherPlayerField(field1, field2, positions)) : "Judge doesn't see difference between players";
     }
+
+    @Test
+    public void testIfJudgeCheckOnlyTheSameCharacters() {
+        Judge judge = new Judge(new Size(15, 15));
+        Field field1 = new NotEmptyField(new Position(1, 1));
+        Field field2 = new NotEmptyField(new Position(0 ,0));
+        Field field3 = new NotEmptyField(new Position(0, 1));
+        Positions positions = new Positions(10, new PositionComparator());
+        positions.add(field1.getPosition());
+        positions.add(field2.getPosition());
+        positions.add(field3.getPosition());
+        Player playerO = new Player("O", Characters.NAUGHT);
+        Player playerX = new Player("X", Characters.CROSS);
+        positions.linkPlayerWithPositions(playerO, field1.getPosition());
+        positions.linkPlayerWithPositions(playerX, field2.getPosition());
+        positions.linkPlayerWithPositions(playerO, field3.getPosition());
+        int neighbours = judge.howManyNeighbours();
+        assert (neighbours == 1) : "Judge ";
+    }
 }
