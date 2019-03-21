0fcefba82b76c4121582ce044d3270ae66a1555d
Ola <aleksandra.podorska@gmail.com>
2019-03-21 09:29:26 +0100

    fix comparison of positions

--- src/main/java/com/firm/OX/Judge.java
+++ src/main/java/com/firm/OX/Judge.java
@@ -19,42 +19,42 @@ class Judge {
         Map<Position, Player> playerMap = positions.findplayerPositions();
         if (playerMap.containsKey(position)){
             if (position.getRow() != size.getLength()){
-                if (playerMap.containsKey(new Position(position.getRow() + 1, position.getColumn()))){
+                if (playerMap.containsKey((new Position((position.getRow() + 1), position.getColumn())))){
                     hasNeighbour = true;
                 }
             }
             if (position.getRow() != size.getLength() && position.getColumn() != size.getHeight()){
-                if (playerMap.containsKey(new Position(position.getRow() + 1, position.getColumn() + 1))){
+                if (playerMap.containsKey(new Position((position.getRow() + 1), (position.getColumn() + 1)))){
                     hasNeighbour = true;
                 }
             }
             if (position.getRow() != size.getLength() && position.getColumn() != 0){
-                if (playerMap.containsKey(new Position(position.getRow() + 1, position.getColumn() - 1))){
+                if (playerMap.containsKey(new Position((position.getRow() + 1), (position.getColumn() - 1)))){
                     hasNeighbour = true;
                 }
             }
             if (position.getColumn() != size.getHeight()){
-                if (playerMap.containsKey(new Position(position.getRow(), position.getColumn() + 1))){
+                if (playerMap.containsKey(new Position(position.getRow(), (position.getColumn() + 1)))){
                     hasNeighbour = true;
                 }
             }
             if (position.getColumn() != 0){
-                if (playerMap.containsKey(new Position(position.getRow(), position.getColumn() - 1))){
+                if (playerMap.containsKey(new Position(position.getRow(), (position.getColumn() - 1)))){
                     hasNeighbour = true;
                 }
             }
             if (position.getRow() != 0){
-                if (playerMap.containsKey(new Position(position.getRow() - 1, position.getColumn()))){
+                if (playerMap.containsKey(new Position((position.getRow() - 1), position.getColumn()))){
                     hasNeighbour = true;
                 }
             }
             if (position.getRow() != 0 && position.getColumn() != size.getHeight()){
-                if (playerMap.containsKey(new Position(position.getRow() - 1, position.getColumn() + 1))){
+                if (playerMap.containsKey(new Position((position.getRow() - 1), (position.getColumn() + 1)))){
                     hasNeighbour = true;
                 }
             }
             if (position.getRow() != 0 && position.getColumn() != 0){
-                if (playerMap.containsKey(new Position(position.getRow() - 1, position.getColumn() - 1))){
+                if (playerMap.containsKey(new Position((position.getRow() - 1), (position.getColumn() - 1)))){
                     hasNeighbour = true;
                 }
             }
--- src/main/java/com/firm/OX/Position.java
+++ src/main/java/com/firm/OX/Position.java
@@ -1,5 +1,7 @@
 package com.firm.OX;
 
+import java.util.Objects;
+
 public class Position {
 
     private int row;
@@ -16,5 +18,19 @@ public class Position {
 
     public int getColumn() {
         return column;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position = (Position) o;
+        return row == position.row &&
+                column == position.column;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(row, column);
     }
 }
